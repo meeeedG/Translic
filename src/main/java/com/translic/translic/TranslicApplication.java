13fae3235a80329bfa2ee49cf088bf478a269067
mohamed <mohamedghalib157@gmail.com>
2026-04-09 10:30:47 +0100

    initialize

---
+++ src/main/java/com/translic/translic/TranslicApplication.java
@@ -0,0 +1,13 @@
+package com.translic.translic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TranslicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/translic/translic/TranslicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.translic.translic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
